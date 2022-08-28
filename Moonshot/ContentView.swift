738a891c40eef0d2a9821902c3c8d0ae3a39baf8
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2022-08-27 18:33:20 -0700

    scroll data

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,16 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("example")
-                .resizable()
-                .scaledToFit()
-//                .scaledToFill()
-                .frame(width: geo.size.width * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
-            //            .clipped()
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
